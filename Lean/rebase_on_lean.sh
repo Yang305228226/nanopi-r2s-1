0de5e3413d54d078834b3da501e547aa606e6456
huang-hw <46146075+huang-hw@users.noreply.github.com>
2020-04-14 10:51:06 +0800

    Update rebase_on_lean.sh

--- Lean/rebase_on_lean.sh
+++ Lean/rebase_on_lean.sh
@@ -2,17 +2,21 @@
 cd friendlywrt-rk3328
 find device/ -name distfeeds.conf -delete
 cd friendlywrt
+echo "==========git config==========="
 git config --global user.email "action@github.com" && git config --global user.name "GitHub Action"
+echo "==========git set_repo_hash==========="
 ../../set_repo_hash.sh Lean https://github.com/coolsnowwolf/lede.git
+echo "==========git add==========="
 git remote add upstream https://github.com/coolsnowwolf/lede.git && git fetch upstream
+echo "==========git rebase==========="
 git rebase adc1a9a3676b8d7be1b48b5aed185a94d8e42728^ --onto upstream/master -X ours
-echo ""
+echo "==========git status==========="
 git status
-echo ""
+echo "==========git checkout feeds==========="
 git checkout upstream/master -- feeds.conf.default
+echo "==========git checkout mac==========="
 git checkout upstream/master -- package/kernel/mac80211/files/lib/netifd/mac80211.sh
-echo ""
+echo "==========git status==========="
 git status
-echo ""
-echo "feeds.conf.default"
+echo "==========show feeds.conf.default==========="
 cat feeds.conf.default
