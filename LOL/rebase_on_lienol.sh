038047c86926532c26c3fb5a885d6f8d50919800
huang-hw <46146075+huang-hw@users.noreply.github.com>
2020-04-14 11:07:31 +0800

    Update rebase_on_lienol.sh

--- LOL/rebase_on_lienol.sh
+++ LOL/rebase_on_lienol.sh
@@ -2,20 +2,27 @@
 cd friendlywrt-rk3328
 find device/ -name distfeeds.conf -delete
 cd friendlywrt
+echo "=======git config======="
 git config --global user.email "action@github.com" && git config --global user.name "GitHub Action"
+echo "=======git remote======="
 git remote add upstream https://github.com/Lienol/openwrt.git && git fetch upstream
+echo "=======git checkout======="
 git checkout upstream/dev-lean-lede -b tmp
+echo "=======git set_repo_hash.h======="
 ../../set_repo_hash.sh Lienol
+echo "=======git commit======="
 git mv README.md README && git commit -m 'undo rename readme for rebasing'
+echo "=======git checkout======="
 git checkout origin/master-v19.07.1
+echo "=======git rebase======="
 git rebase adc1a9a3676b8d7be1b48b5aed185a94d8e42728^ --onto tmp -X ours
-echo ""
+echo "=======git status======="
 git status
-echo ""
+echo "=======git check1======="
 git checkout upstream/dev-lean-lede -- feeds.conf.default
+echo "=======git check2======="
 git checkout upstream/dev-lean-lede -- package/kernel/mac80211/files/lib/netifd/mac80211.sh
-echo ""
+echo "=======git status======="
 git status
-echo ""
-echo "feeds.conf.default"
+echo "=======feeds.conf.default======="
 cat feeds.conf.default
